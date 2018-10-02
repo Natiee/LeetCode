ba8863c87c7871348ee1c18171feac7ac8c35305
2471374200@qq.com <2471374200@qq.com>
2018-10-02 22:12:44 +0800

    all content

--- LeetCode/src/0125-Valid-Palindrome/0125.py
+++ LeetCode/src/0125-Valid-Palindrome/0125.py
@@ -4,9 +4,9 @@ class Solution:
         :type s: str
         :rtype: bool
         """
-        # 将字符串中的字母按顺序拿出来
-        # 并且将大写字母转化成小写添加到一个新的字符串中
-        # 判断是否是回文字符串
+        # 字符串中的字母按顺序拿出来
+        # 将大写的字母转化为小写字母添加到一个新的字符串中
+        # 判断回文字符串
         s_filter = ''.join(filter(str.isalnum,s)).lower()
         return s_filter[::-1] == s_filter
 
@@ -14,4 +14,3 @@ class Solution:
 if __name__ == '__main__':
     a = "A man, a plan, a canal: Panama"
     print(Solution().isPalindrome(a))
-
