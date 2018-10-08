384fd0cc4f38ad3188d452d311a115962d85c5ba
2471374200@qq.com <2471374200@qq.com>
2018-10-08 20:33:51 +0800

    all content

---
+++ src/0009-Palindrome_Number/0009.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        return 'x' == 'x'[::-1]
+
+
+if __name__ == '__main__':
+    x = 121
+    print(Solution().isPalindrome(x))
